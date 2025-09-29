59be681bbf500d9bd766344eec11bfe63ad98a02
Ali Mahmood Rana <159713825+AliRana30@users.noreply.github.com>
2025-09-30 00:21:26 +0500

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -30,13 +30,13 @@ const HeroSection = () => {
         className="w-full h-full object-cover blur-sm"
       />
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white bg-black bg-opacity-40">
-        <h1 className="text-4xl md:text-6xl mb-4 min-h-[1.2em]">
+        <h1 className="text-3xl absolute left-7 top-40  md:text-6xl mb-4 min-h-[1.2em]  font-bold">
           {displayText}
           <span className="animate-pulse">|</span>
         </h1>
         <button
           onClick={() => navigate("/products")}
-          className="mt-14 group relative px-6 py-2 bg-white text-black text-lg font-semibold tracking-wider transition-all duration-500 hover:bg-black hover:text-white border-2 border-white hover:border-white overflow-hidden"
+          className="mt-14 group relative px-6 top-20 py-2 bg-white text-black text-lg font-semibold tracking-wider transition-all duration-500 hover:bg-black hover:text-white border-2 border-white hover:border-white overflow-hidden"
         >
           <span className="relative z-10">Shop Now</span>
           <div className="absolute inset-0 bg-black transform scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></div>
