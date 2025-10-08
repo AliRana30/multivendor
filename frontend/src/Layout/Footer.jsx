f6699ac68080b397cbd6860673f68a22247c945b
Ali Mahmood Rana <159713825+AliRana30@users.noreply.github.com>
2025-10-08 20:22:33 +0500

    Update Footer.jsx

--- frontend/src/Layout/Footer.jsx
+++ frontend/src/Layout/Footer.jsx
@@ -15,24 +15,6 @@ import {
 const Footer = () => {
   return (
     <div className="bg-[#000] text-white">
-      {/* Subscribe Bar */}
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
-        <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-[#56d879]">Subscribe</span> us for latest news <br /> events and offers
-        </h1>
-        <div className="flex flex-col sm:flex-row items-center">
-          <input
-            type="text"
-            required
-            placeholder="Enter your email..."
-            className="text-gray-800 sm:w-72 w-full sm:mr-5 mr-0 mb-4 sm:mb-0 py-2.5 rounded px-2 focus:outline-none"
-          />
-          <button className="bg-[#56d879] hover:bg-teal-500 duration-300 px-5 py-2.5 rounded-md text-white md:w-auto w-full">
-            Submit
-          </button>
-        </div>
-      </div>
-
       {/* Main Footer Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
         {/* Logo & Social */}
@@ -114,3 +96,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
