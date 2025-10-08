87eae84c9e14221a5cc91c2880e1ef31ec2ced6f
Ali Mahmood Rana <159713825+AliRana30@users.noreply.github.com>
2025-10-09 00:07:44 +0500

    Update axiosCongif.js

--- frontend/src/components/axiosCongif.js
+++ frontend/src/components/axiosCongif.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://multivendors-7cy2.onrender.com/api",
+  baseURL: import.meta.env.VITE_SERVER || "http://localhost:5000/api", 
   withCredentials: true,
 });
 
 export default api;
-
